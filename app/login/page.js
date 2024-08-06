a8dfa80f318ab89a14f8d14f88b40a6db6ec6023
addymwenda12 <muneneaddy@gmail.com>
2024-08-06 11:59:19 +0300

    Implemented simple login page

--- app/login/page.js
+++ app/login/page.js
@@ -2,10 +2,9 @@ import LoginForm from './components/LoginForm';
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen bg-gradient-radial from-blue-400 to-purple-500 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
-        <LoginForm />
-      </div>
+    <div className="container mx-auto mt-10">
+      <h1 className="text-3xl font-bold mb-5 text-center">Login</h1>
+      <LoginForm />
     </div>
   );
 }
