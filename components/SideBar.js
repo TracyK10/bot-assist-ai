d65d30cf39672a4adeeb22d4346c06c953b7b1cf
addymwenda12 <muneneaddy@gmail.com>
2024-08-06 18:15:18 +0300

    Added SideBar component to highlight active chat item

---
+++ components/SideBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "@/styles/sidebar.module.css";
+
+const Sidebar = ({ chatHistory, activeChatId, OnNewChat, OnChatSelect }) => {
+  return (
+    <div className={styles.sidebar}>
+      <button onClick={OnNewChat} className={styles.newChatButton}>
+        New Chat
+      </button>
+      <div className={styles.chatList}>
+        {chatHistory.map((chat) => (
+          <div
+            key={chat.id}
+            onClick={() => OnChatSelect(chat.id)}
+            className={
+              `${styles.chatItem} ${activeChatId === chat.id ? styles.active : ''}`
+            }
+          >
+            {chat.title}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
